a4ec127509dd8b413cbd1f028d653f0dcd5fd335
Maximusikk <78302672+Maximusikk@users.noreply.github.com>
2024-01-09 10:41:33 +0500

    Update mm2snow.lua

--- mm2snow.lua
+++ mm2snow.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Maximusikk/itrScri/main/mains.lua", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Maximusikk/itrScri/main/discordtxt.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Maximusikk/itrScri/main/mains.lua", true))()
